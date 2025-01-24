291e4be7ac54081179da67e0b7881d51050b0577
Christiana Ola <c.ola@alustudent.com>
2025-01-24 12:39:46 +0100

    updated

--- donation_app/urls.py
+++ donation_app/urls.py
@@ -2,11 +2,12 @@
 
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('donate-food/', views.donate_food, name='donate_food'),  # This renders the form
-    path('submit-donation/', views.submit_donation, name='submit_donation'),  # This handles form submission
+    path('donate-food/', views.donate_food, name='donate_food'),
+    path('submit-donation/', views.submit_donation, name='submit_donation'),
     path('agreement/', views.agreement, name='agreement'),
     path('confirmation/', views.confirmation, name='confirmation'),
 
@@ -16,8 +17,13 @@ urlpatterns = [
     path('get-donations/', views.get_donations, name='get_donations'),
 
     # Non Profit
-    path('non-profit/', views.non_profit_dashboard, name='non_profit_dashboard'),
+    path('non-profit-dashboard/', views.non_profit_dashboard, name='non_profit_dashboard'),
 
     # Logistics
     path('logistics/', views.logistics_dashboard, name='logistics_dashboard'),
+
+    # Auth
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
